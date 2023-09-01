e7f56e3fd61bc482d2dd3171e2c5be4869903532
MalconH <malcon.heltner@gmail.com>
2023-09-01 16:39:45 -0300

    Actualiza la clase 8
    Estos cambios son de seguir la clase 8 en el curso de YouTube

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-const $form = document.formulario;
-const nombre = $form.nombre.value;
-const ciudad = $form.ciudad.value;
-const comportamiento = $form.comportamiento.value;
-const descripcionRegalo = $form["descripcion-regalo"].value;
-
 function validarNombre(nombre) {
     if (nombre.length === 0) {
         return "Este campo debe tener al menos 1 caracter";
@@ -13,5 +7,111 @@ function validarNombre(nombre) {
         return "Este campo debe tener menos de 50 caracteres";
     }
 
+    if (!/^[a-z]+$/i.test(nombre)) {
+        return "Este campo sólo acepta letras";
+    }
+
     return "";
 }
+
+function validarCiudad(ciudad) {
+    if (ciudad.length === 0) {
+        return "La ciudad no puede estar vacía";
+    }
+
+    return "";
+}
+
+function validarDescripcionRegalo(descripcionRegalo) {
+    if (descripcionRegalo.length >= 100) {
+        return "La descripción no puede tener más de 100 caracteres";
+    }
+
+    if (descripcionRegalo.length === 0) {
+        return "La descripción no puede estar vacía";
+    }
+
+    if (!/^[a-z0-9]+$/i.test(descripcionRegalo)) {
+        return "La descripción sólo puede tener números y letras";
+    }
+
+    return "";
+}
+
+function validarFormulario(event) {
+    const $form = document.formulario;
+
+    const nombre = $form.nombre.value;
+    const ciudad = $form.ciudad.value;
+    const descripcionRegalo = $form["descripcion-regalo"].value;
+
+    const errorNombre = validarNombre(nombre);
+    const errorCiudad = validarCiudad(ciudad);
+    const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
+
+    const errores = {
+        nombre: errorNombre,
+        ciudad: errorCiudad,
+        "descripcion-regalo": errorDescripcionRegalo
+    };
+
+    manejarErrores(errores);
+
+    event.preventDefault();
+}
+
+function manejarErrores(errores) {
+    const keys = Object.keys(errores);
+    const $errores = document.querySelector("#errores");
+
+    borrarErrores();
+
+    keys.forEach(function (key) {
+        const error = errores[key];
+        const $error = document.createElement("li");
+
+        if (error) {
+            agregarError(error);
+            $form[key].className = "error";
+        } else {
+            $form[key].className = "";
+        }
+    });
+
+    // const errorNombre = errores.nombre;
+    // const errorCiudad = errores.ciudad;
+    // const errorDescripcionRegalo = errores.descripcionRegalo;
+
+    // if (errorNombre) {
+    //     $form.nombre.className = "error";
+    // } else {
+    //     $form.nombre.className = "";
+    // }
+
+    // if (errorCiudad) {
+    //     $form.ciudad.className = "error";
+    // } else {
+    //     $form.ciudad.className = "";
+    // }
+
+    // if (errorDescripcionRegalo) {
+    //     $form["descripcion-regalo"].className = "error";
+    // } else {
+    //     $form["descripcion-regalo"].className = "";
+    // }
+}
+
+function agregarError(error) {
+    const $errores = document.querySelector("#errores");
+    const $error = document.createElement("li");
+
+    $error.textContent = error;
+    $errores.appendChild($error);
+}
+
+function borrarErrores() {
+    document.querySelector("#errores").innerHTML = "";
+}
+
+const $form = document.querySelector("#carta-a-santa");
+$form.onsubmit = validarFormulario;
--- js/pruebas.js
+++ js/pruebas.js
@@ -1,10 +1,59 @@
 function probarValidarNombre() {
     console.assert(validarNombre("") === "Este campo debe tener al menos 1 caracter",
-        "ValidarNombre no validó que el nombre sea un string vacío");
+        "ValidarNombre no validó que el nombre sea un string vacío"
+    );
 
     console.assert(validarNombre("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") ===
         "Este campo debe tener menos de 50 caracteres",
-        "ValidarNombre no validó que el nombre tenga más de 50 caracteres")
+        "ValidarNombre no validó que el nombre tenga más de 50 caracteres"
+    );
+
+    console.assert(validarNombre("1235") === "Este campo sólo acepta letras",
+        "validarNombre no valido que el nombre sólo puede contener letras"
+    );
+
+    // Hay que tambiar crear UnitTests para el happy path (ver apunte 31/08)
+    console.assert(validarNombre("Malcon") === "",
+        "validarNombre falló validando un nombre correcto"
+    );
 }
 
+function probarValidarCiudad() {
+    console.assert(
+        validarCiudad("") === "La ciudad no puede estar vacía",
+        "validarCiudad no validó que la ciudad no puede estar vacía"
+    );
+
+    // Happy path (apunte)
+    console.assert(
+        validarCiudad("Buenos Aires") === "",
+        "validarCiudad no validó correctamente una ciudad válida"
+    );
+}
+
+function probarValidarDescripcionRegalo() {
+    console.assert(validarDescripcionRegalo("") === "La descripción no puede estar vacía",
+        "validarDescripcionRegalo no validó que la descripción no puede ser un string vacio"
+    );
+
+    console.assert(
+        validarDescripcionRegalo("asdasdasdasdasdadasdasdasdasdasdadasdasdasdasdasdadasdasdasdasdasdadasdasdasdasdasdadasdasdasdasdasdadasdasdasdasdasdadasdasdasdasdasdadasdasdasdasdasdad"
+        ) === "La descripción no puede tener más de 100 caracteres",
+        "validarDescripcionRegalo no validó que la descripción no puede ser igual o mayor a 100 caracteres"
+    );
+
+    console.assert(
+        validarDescripcionRegalo(".,.,.,.,") === "La descripción sólo puede tener números y letras",
+        "validarDescripcionRegalo no validó que la descripción sólo puede tener números y letras"
+    );
+
+    // Hay que también crear UnitTests para el "happy path" (camino correcto).
+    console.assert(
+        validarDescripcionRegalo("Regalo") === "",
+        "validarDescripcionRegalo falló al validar una descripción correcta"
+    );
+}
+
+
 probarValidarNombre();
+probarValidarDescripcionRegalo();
